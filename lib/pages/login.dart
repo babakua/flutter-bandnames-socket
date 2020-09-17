556f7f353d905eaa7d11e778d11ef0109b24c32c
babakua <hectordelarosa@msn.com>
2020-09-17 10:10:26 -0400

    --- Auto-Git Commit ---

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,65 +17,68 @@ class _LoginState extends State<Login> {
       right: true,
       bottom: true,
       child: Scaffold(
-        body: Column(
-          children: [
-            Text('login'),
-            FlatButton(
-              child: Text('Entrar'),
-              onPressed: () {
-                print('test');
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('login'),
+              FlatButton(
+                child: Text('Entrar'),
+                onPressed: () {
+                  print('test');
 
-                //  Navigator.push(context, route());\\
-                // Navigator.push(context,MaterialPageRoute(builder: (context) => NavDrawerExample());
+                  //  Navigator.push(context, route());\\
+                  // Navigator.push(context,MaterialPageRoute(builder: (context) => NavDrawerExample());
 
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => new NavDrawerExample()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          new NavDrawerExample()));
 
-                //De esta forma no podemos realizar un back a la pagina anterior
-                // buildPushReplacementNamed(context);
-              },
-            ),
-            Center(
-              child: Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    logo(),
-                    Text('Usuario'),
-                    TextField(),
-                    Text('Clave'),
-                    TextField(
-                      obscureText: true,
-                    )
+                  //De esta forma no podemos realizar un back a la pagina anterior
+                  // buildPushReplacementNamed(context);
+                },
+              ),
+              Center(
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      logo(),
+                      Text('Usuario'),
+                      TextField(),
+                      Text('Clave'),
+                      TextField(
+                        obscureText: true,
+                      )
 
 /*
-                    
-                    const ListTile(
-                      leading: Icon(Icons.album),
-                      title: Text('The Enchanted Nightingale'),
-                      subtitle:
-                          Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-                    ),
-                    ButtonBar(
+                      
+                      const ListTile(
+                        leading: Icon(Icons.album),
+                        title: Text('The Enchanted Nightingale'),
+                        subtitle:
+                            Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                      ),
+                      ButtonBar(
 
-                      children: <Widget>[
-                        FlatButton(
-                          child: const Text('BUY TICKETS'),
-                          onPressed: () {/* ... */},
-                        ),
-                        FlatButton(
-                          child: const Text('LISTEN'),
-                          onPressed: () {/* ... */},
-                        ),
-                      ],
-                    ),
+                        children: <Widget>[
+                          FlatButton(
+                            child: const Text('BUY TICKETS'),
+                            onPressed: () {/* ... */},
+                          ),
+                          FlatButton(
+                            child: const Text('LISTEN'),
+                            onPressed: () {/* ... */},
+                          ),
+                        ],
+                      ),
 
 */
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
